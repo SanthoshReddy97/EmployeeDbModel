6a41e52a6ed91ed718bd5b0c295e9b194f9e4595
Ubuntu <ubuntu@ip-172-31-35-64.us-east-2.compute.internal>
2019-09-26 06:07:12 +0000

    updated

--- app.js
+++ app.js
@@ -20,8 +20,19 @@ app.set('views', path.join(__dirname + '/views/'));
 app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainlayout', layoutsDir:__dirname + '/views/' }));
 app.set('view engine', 'hbs');
 
+app.get("/", (req, res) => {
+res.status(200).send({"msg": "Employees Database"})
+});
 employeeController(app);
 
+
+app.get("/hi", (req, res) => {
+    res.status(200).send({"msg": "hellow world"})
+});
+
+app.get("/hello", (req, res) => {
+res.status(200).send({"msg":"hi everyone"})
+});
 app.listen(3000, () => {
     console.log('you are listening to the port 3000')
 }) ;
--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose  = require('mongoose');
 
 
 
-mongoose.connect('mongodb+srv://newtest:newtest@cluster0-u9eoi.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
+mongoose.connect('mongodb+srv://myprofile:12345@connecttoproject-u9eoi.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
     if(!err) {console.log("mongodb connection successful")}
     else {console.log("mongodb connection failed")}
 });
